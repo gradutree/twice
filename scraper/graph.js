5050656f2862dacbcd72ece59d1b5a129a18ea5a
danny <hyeong.yu@mail.utoronto.ca>
2017-03-06 21:10:48 -0500

    Graph
    Graph part added

---
+++ scraper/graph.js
@@ -0,0 +1,132 @@
+$(function(){ // on dom ready
+
+
+
+var cy = cytoscape({
+  container: document.getElementById('cy'),
+
+  boxSelectionEnabled: false,
+  autounselectify: true,
+
+  style: cytoscape.stylesheet()
+    .selector('node')
+      .css({
+        'content': 'data(id)'
+      })
+    .selector('edge')
+      .css({
+        'target-arrow-shape': 'triangle',
+        'width': 4,
+        'line-color': '#ddd',
+        'target-arrow-color': '#ddd',
+        'curve-style': 'bezier'
+      })
+
+    .selector('.highlighted')
+      .css({
+        'background-color': '#61bffc',
+        'line-color': '#61bffc',
+        'target-arrow-color': '#61bffc',
+        'transition-property': 'background-color, line-color, target-arrow-color',
+        'transition-duration': '0.5s'
+      }),
+
+
+  
+  elements: {
+      nodes: [
+        { data: { id: 'a' ,degree: 1} },
+        { data: { id: 'b' ,degree: 1} },
+        { data: { id: 'c' ,degree: 1} },
+        { data: { id: 'd' ,degree: 1} },
+        { data: { id: 'e' ,degree: 1} },
+        { data: { id: 'f' ,degree: 2} },
+        { data: { id: 'g' ,degree: 2} },
+        { data: { id: 'h' ,degree: 2} },
+        { data: { id: 'i' ,degree: 3} }
+      ], 
+      
+      edges: [
+        { data: { id: 'af', weight: 1, source: 'a', target: 'f' } },
+        { data: { id: 'bf', weight: 1, source: 'b', target: 'f' } },
+        { data: { id: 'bg', weight: 1, source: 'b', target: 'g' } },
+        { data: { id: 'cg', weight: 1, source: 'c', target: 'g' } },
+        { data: { id: 'ch', weight: 1, source: 'c', target: 'h' } },
+        { data: { id: 'dh', weight: 1, source: 'd', target: 'h' } },
+        { data: { id: 'eh', weight: 1, source: 'e', target: 'h' } },
+        { data: { id: 'fi', weight: 1, source: 'f', target: 'i' } },
+        { data: { id: 'gi', weight: 1, source: 'g', target: 'i' } },
+        { data: { id: 'hi', weight: 1, source: 'h', target: 'i' } }
+      ]
+    },
+
+
+  
+  layout: {
+    name: 'breadthfirst',
+    directed: true,
+    roots: '#a, #b, #c, #d, #e', 
+    padding: 10
+  }
+
+  
+
+});
+
+
+
+
+
+//highlight the node
+cy.on('tap', function(evt){
+  if(evt.cyTarget===cy || evt.cyTarget.isEdge()) return;
+  var tapid= cy.$('#'+evt.cyTarget.id());
+/*  if(tapid.hasClass('highlighted')) tapid.removeClass('highlighted');
+  else{
+    tapid.addClass('highlighted');
+  }*/
+  findconnected(tapid); 
+});
+
+
+
+
+var findconnected = function(node){
+
+    var i = 1;
+    var connectedEdges = node.connectedEdges();
+    var roots = cy.nodes().roots();
+    
+    roots.forEach(function(e) {
+      if(e.id()==node.id()) node.addClass('highlighted');
+    })
+
+    connectedEdges.forEach(function(ele){
+        var target = ele.target();
+        var source = ele.source();
+    
+        if(source.hasClass('highlighted')){
+            i++;
+            return;
+        }
+            
+        if(i==connectedEdges.size()){
+            ele.edgesTo
+        }
+
+
+    });
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}); // on dom ready
