c6b3e7aa78b6d1567f352c580e7f8cbdd4c5d432
Mark Anthony Viola <mark.anthony.viola@gmail.com>
2017-03-12 20:23:20 -0400

    Moved search styling to css file

--- react/search.jsx
+++ react/search.jsx
@@ -4,38 +4,6 @@ import { render } from 'react-dom';
 import SearchResult from "./components/searchResult.jsx";
 import model from "../frontend/static/js/app/model.js"
 
-// const tabStyle = {
-// 	display: 'flex',
-// 	flexDirection: 'row',
-// };
-
-const headerStyle = {
-	flexGrow: 10,
-};
-
-// const searchDivStyle = {
-// 	flexGrow: 1,
-// 	display: 'flex',
-// 	flexDirection: 'column',
-// };
-
-const inputStyle = {
-	flexGrow: 1,
-};
-
-const dropdownStyle = {
-	marginTop: '5px',
-	flexGrow: 1,
-};
-
-const resultStyle = {
-	padding: '10px',
-};
-
-const resultHeaderStyle = {
-	marginBottom: '10px',
-};
-
 class Search extends Component {
 	constructor(){
 		super();
@@ -69,18 +37,18 @@ class Search extends Component {
 		var msg = "Search for " + this.state.school + " courses";
 		return (<div>
 					<div className="search_banner"> 
-						<h3 style={headerStyle}> {msg} </h3>
+						<h3 className="search_header"> {msg} </h3>
 						<div className="search_div">
-							<input style={inputStyle} onChange={this.changeResults.bind(this)} placeholder="Search for course" />
-							<select style={dropdownStyle} value={this.state.school} onChange={this.changeSchool.bind(this)} >
+							<input className="search_input" onChange={this.changeResults.bind(this)} placeholder="Search for course" />
+							<select className="search_dropdown" value={this.state.school} onChange={this.changeSchool.bind(this)} >
 								<option value="UTSC">UTSC</option>
 							  	<option value="UTSG">UTSG</option>
 							  	<option value="UTM">UTM</option>
 							</select>
 						</div>
 					</div>
-					<div style={resultStyle}>
-						<h3 style={resultHeaderStyle}>Search Results</h3>
+					<div className="search_result">
+						<h3 id="search_result_header">Search Results</h3>
 						{this.state.results}
 					</div>
 				</div>)
