2d1bd49c9bc30c5d82fdd2b3d1bbb9392f8fe2f2
ZiXian(Wannie) Chen <Wannie923@gmail.com>
2017-03-21 14:57:39 -0400

    popup fix

--- frontend/design/popup.js
+++ frontend/design/popup.js
@@ -10,3 +10,11 @@
 			popover.classList.add("hidden");
 		}
 	});
+	
+	document.addEventListener("click", function(e) {
+		var popover = document.getElementById("popover-arrow");
+		var menu = document.getElementById("menu");
+		if ((!menu.contains(e.target) && !document.getElementById("profile").contains(e.target)) || e.target == menu) {
+			popover.classList.add("hidden");
+		}
+	});
