977c88ffe67f74566f3c9fa0c34692f5b91c165b
ZiXian(Wannie) Chen <Wannie923@gmail.com>
2017-03-25 20:13:51 -0400

    new nav

--- react/app.jsx
+++ react/app.jsx
@@ -54,6 +54,11 @@ class NavTabs extends Component {
 	render() {
 		return (<header className="header_bar">
 					<nav className="sub_nav">
+						<div className="flex-row">
+							<Link to="/dashboard"><div role="nav" className={"nav_pill"+(this.props.value == 0 ? " pill_active" : "")}>Dashboard</div></Link>
+							<Link to="/trees"><div role="nav" className={"nav_pill"+(this.props.value == 1 ? " pill_active" : "")}>Your Trees</div></Link>
+							<Link to="/search"><div role="nav" className={"nav_pill"+(this.props.value == 2 ? " pill_active" : "")}>Search courses</div></Link>
+						</div>
 					</nav>
 		</header>);
 	}
